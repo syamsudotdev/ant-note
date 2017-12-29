1927209ccc8a26c9e4536340437162278ba13165
samsunoor <samsu.noor@gmail.com>
2017-12-29 10:09:43 +0700

    fix get db instance

--- app/src/main/java/net/mnsam/antnote/main/database/NoteRoomDatabase.kt
+++ app/src/main/java/net/mnsam/antnote/main/database/NoteRoomDatabase.kt
@@ -11,19 +11,22 @@ import net.mnsam.antnote.main.database.model.Note
  * Created by Mochamad Noor Syamsu on 12/27/17.
  */
 
-@Database(entities = [Note::class], version = 1)
-abstract class NoteRoomDatabase(val context: Context) : RoomDatabase() {
+@Database(entities = [(Note::class)], version = 1)
+abstract class NoteRoomDatabase : RoomDatabase() {
     abstract var noteDao: NoteDao
-    private var INSTANCE: NoteRoomDatabase? = null
 
-    fun getDatabase(): NoteRoomDatabase? {
-        if (INSTANCE === null)
-            synchronized(NoteRoomDatabase::class) {
-                if (INSTANCE === null)
-                    INSTANCE = Room.databaseBuilder(context.applicationContext,
-                            NoteRoomDatabase::class.java,
-                            "note_database").build()
-            }
-        return INSTANCE
+    companion object {
+        private var INSTANCE: NoteRoomDatabase? = null
+
+        fun getDatabase(context: Context): NoteRoomDatabase? {
+            if (INSTANCE === null)
+                synchronized(NoteRoomDatabase::class) {
+                    if (INSTANCE === null)
+                        INSTANCE = Room.databaseBuilder(context.applicationContext,
+                                NoteRoomDatabase::class.java,
+                                "ant_note").build()
+                }
+            return INSTANCE
+        }
     }
 }
--- app/src/main/java/net/mnsam/antnote/main/database/repository/NoteRepository.kt
+++ app/src/main/java/net/mnsam/antnote/main/database/repository/NoteRepository.kt
@@ -10,12 +10,12 @@ import net.mnsam.antnote.main.database.model.Note
 /**
  * Created by Mochamad Noor Syamsu on 12/27/17.
  */
-class NoteRepository(val application: Application) {
-    private val noteDao: NoteDao?
+class NoteRepository(application: Application) {
+    private val noteDao: NoteDao
     private val allNotes: LiveData<MutableList<Note>>
 
     init {
-        val db: NoteRoomDatabase? = NoteRoomDatabase(application) {}.getDatabase()
+        val db: NoteRoomDatabase = NoteRoomDatabase.getDatabase(application.baseContext)!!
         noteDao = db.noteDao
         allNotes = noteDao.getAllNotes()
     }
@@ -24,10 +24,9 @@ class NoteRepository(val application: Application) {
         insertAsyncTask(noteDao).execute(note)
     }
 
-    private class insertAsyncTask constructor(private val mAsyncTaskDao: NoteDao) : AsyncTask<Note, Void, Void>() {
-
+    private class insertAsyncTask constructor(private val asyncTaskDao: NoteDao) : AsyncTask<Note, Void, Void>() {
         override fun doInBackground(vararg params: Note): Void? {
-            mAsyncTaskDao.insert(params[0])
+            asyncTaskDao.insert(params[0])
             return null
         }
     }
